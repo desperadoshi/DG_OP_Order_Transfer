2acdec356826df399b43b1aa67464a9ebe0c91d8
JingchangShi <jingchangshi@gmail.com>
2019-04-07 21:53:40 +0800

    add prolongation of the restricted solutions.

--- main.py
+++ main.py
@@ -74,26 +74,54 @@ p1_order = 2
 p2_order = 3
 N_mat = calc_matrix_MN(p1_order, p2_order)
 
-#  M_mat = np.asmatrix(M_mat)
-#  N_mat = np.asmatrix(N_mat)
-
-#  print(M_mat)
-#  print(N_mat)
-
 I_mat = np.matmul(np.linalg.inv(M_mat),N_mat)
+print("I_mat is")
 print(I_mat)
 
 x_arr1 = getSolutionPoints(p1_order)
 x_arr2 = getSolutionPoints(p2_order)
 
+print("Solution points of P%d are"%(p1_order))
 print(x_arr1)
+print("Solution points of P%d are"%(p2_order))
 print(x_arr2)
 
-u_arr1 = x_arr1**2
-u_arr2 = x_arr2**2
+u_arr1 = x_arr1**3
+u_arr2 = x_arr2**3
 
+print("Origninal solutions of P%d are"%(p1_order))
 print(u_arr1)
+print("Origninal solutions of P%d are"%(p2_order))
 print(u_arr2)
 
-print(np.matmul(I_mat,u_arr2))
+u_arr2_1 = np.matmul(I_mat,u_arr2)
+print("Restricted solutions of P%d are"%(p2_order))
+print(u_arr2_1)
 
+# Prolongate the (P-1) order solution from P solution points to (P+1) solution points.
+from numpy.polynomial.polynomial import polyval
+Basis_List1 = getLagrangeBasis(p1_order,x_arr1)
+Basis_List2 = getLagrangeBasis(p2_order,x_arr2)
+n_nodes1 = p1_order + 1
+n_nodes2 = p2_order + 1
+BasisCoef_List1 = [Basis_List1[i].coef for i in range(n_nodes1)]
+BasisCoef_List2 = [Basis_List2[i].coef for i in range(n_nodes2)]
+prolong_mat = np.zeros((n_nodes2, n_nodes1))
+for i_basis_func1 in range(len(BasisCoef_List1)):
+    basis_func1_coef_arr = BasisCoef_List1[i_basis_func1]
+    prolong_mat[:,i_basis_func1] = polyval(x_arr2, basis_func1_coef_arr)
+
+print("Prolongation matrix is")
+print(prolong_mat)
+u_arr2_1_2 = np.matmul(prolong_mat, u_arr2_1)
+print("Prolongation of restricted solutions of P%d are"%(p2_order))
+print(u_arr2_1_2)
+
+# Debug P2
+a = np.array([ [x_arr1[0]**2, x_arr1[0]], [x_arr1[2]**2, x_arr1[2]] ])
+b = np.array([ u_arr2_1[0], u_arr2_1[2] ])
+coef = np.linalg.solve(a, b)
+print("Ordered coefficients of the restricted polynomial are")
+print(coef)
+print("The following array is expected to be the same as the prolongation of the restricted solutions of P2.")
+print(coef[0]*x_arr2**2 + coef[1]*x_arr2)
